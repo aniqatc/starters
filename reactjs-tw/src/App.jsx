0306873ad3260910e16ffd15e233c869806b7cc6
aniqatc <aniqatc97@gmail.com>
2025-01-23 23:06:01 -0500

    upgrade to tailwind v4

--- reactjs-tw/src/App.jsx
+++ reactjs-tw/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       </ul>
       <div>
         <button
-          className="flex items-center gap-1 text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          className="flex items-center gap-1 text-white bg-linear-to-r from-purple-500 to-pink-500 hover:bg-linear-to-l focus:ring-4 focus:outline-hidden focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           onClick={() => setCount((count) => count + 1)}>
           <Alien weight="duotone" size="24" /> click to increase: {count}
         </button>
--- reactjs-tw/vite.config.js
+++ reactjs-tw/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,6 +24,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       includeAssets: [],
       registerType: 'autoUpdate',
